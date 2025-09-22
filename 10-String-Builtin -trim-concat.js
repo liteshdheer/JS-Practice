8f9f6e065f4d5788a5844742f8235efca063b8a5
Litesh Dheer <117711003+liteshdheer@users.noreply.github.com>
2025-09-22 11:18:50 +0000

    Update

---
+++ 10-String-Builtin -trim-concat.js
@@ -0,0 +1,10 @@
+var a = 'Hello World'
+var b = '     Hello World          '
+
+console.log(a);
+console.log('Array - ',a[1]);
+console.log('charAt - ',a.charAt(6));
+console.log('Uppercase - ',a.toUpperCase());
+console.log('Split - ',a.split(' '));
+console.log('concat - ',a.concat(' First Time'));
+console.log('trim - ',b.trim());
