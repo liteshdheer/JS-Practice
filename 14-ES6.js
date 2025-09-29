54574734349cb401b0c76fbe4649136776d63e65
Litesh Dheer <117711003+liteshdheer@users.noreply.github.com>
2025-09-29 07:00:04 +0000

    Es6 Function

---
+++ 14-ES6.js
@@ -0,0 +1,30 @@
+// ES5 Javascript
+
+// var detail = {
+//     name: "Sutherland",
+//     age:50,
+//     location:"Noida"
+// }
+
+// var name = detail.name;
+// var age = detail.age;
+// var location = detail.location;
+
+// console.log(name);
+// console.log(age);
+// console.log(location);
+
+
+// ES6 Javascript
+
+let details = {
+    name: "Litesh",
+    age:14,
+    location:"Delhi"
+}
+
+let {name,age,location} = details;
+
+console.log(name);
+console.log(age);
+console.log(location);
