918e73960edf53d34765183429e6372576b3a0b6
Litesh Dheer <117711003+liteshdheer@users.noreply.github.com>
2025-10-01 11:20:20 +0000

    Promise update

--- 14-ES6.js
+++ 14-ES6.js
@@ -28,3 +28,25 @@ let {name,age,location} = details;
 console.log(name);
 console.log(age);
 console.log(location);
+
+
+// Promisis ES6
+
+const myPromise = new Promise((resolved,rejected) => {
+        let a = 4;
+        let b = 5;
+        let c = a +b;
+
+        if(c === 4){
+            resolved(`Yes!, ${a} + ${b} = 4`);
+        }else {
+            rejected(`No! ${a} + ${b} = 4`);
+        }
+
+    });
+
+    myPromise.then((n) => {
+        console.log(n);
+    }).catch((err) => {console.log(err)});
+
+    
